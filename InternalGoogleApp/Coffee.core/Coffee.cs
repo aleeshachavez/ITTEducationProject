5ac1edd8dd578e973db047772fc446625ff51e9f
aleeshachavez <51204864+aleeshachavez@users.noreply.github.com>
2021-01-18 11:46:49 -0700

    Add files via upload

---
+++ InternalGoogleApp/Coffee.core/Coffee.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Coffee.core
+{
+    public class Coffee
+    {
+        
+        //defining the properties in the Coffee Data, will add more as I think of useful marketing data
+        public int Id { get; set; } //for storing coffee data in a database... eventually 
+        
+        [Required] //makes brandname a requirement, so user cant leave it blank when editing data
+        public string BrandName { get; set; } //for storing name of coffee bag
+        public int Age { get; set; } //age of the consumer
+        
+        [Required] //makes location of purchase required
+        public string Location { get; set; } //location of product purchase
+        public RoastType Roast { get; set; } //for the roast types in RoastType.cs 
+
+
+    }
+}
---
+++ InternalGoogleApp/Coffee.core/RoastType.cs
@@ -0,0 +1,13 @@
+﻿namespace Coffee.core
+{
+    
+    //putting the roast type in separate enum
+        public enum RoastType
+        {
+            None, //default type
+            Light, //roast type light
+            Medium, //roast type Medium
+            Dark
+        }
+    
+}
